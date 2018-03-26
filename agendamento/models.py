aaeb85925e4155b7541bcc115e7d5ffaddc1af57
Adalto Picotti Junior <adalto@localhost.localdomain>
2018-03-26 13:08:49 -0300

    Create Agendamento Model

--- agendamento/models.py
+++ agendamento/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Agendamento(models.Model):
+    cliente = models.CharField(max_length=200, null=False)
+    agencia = models.CharField(max_length=200, null=False)
+    inicio_vigencia = models.DateField(("Date"), blank=False)
+    fim_vigencia = models.DateField(("Date"), blank=False)
+    acao = models.TextField(max_length=500, null=True)
+
+    def __str__(self):
+        return  self.cliente
