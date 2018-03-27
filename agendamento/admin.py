9cd101cf217027a5fb01abf0a35e051265e2f5d9
Adalto Picotti Junior <adalto@localhost.localdomain>
2018-03-27 00:36:01 -0300

    add models

--- agendamento/admin.py
+++ agendamento/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+admin.site.register(Cooperativa)
+admin.site.register(Segurado)
+admin.site.register(Ramo)
+admin.site.register(Produto)
 admin.site.register(Agendamento)
+admin.site.register(Empresarial)
+admin.site.register(VidaGrupo)
+admin.site.register(Frota)
+admin.site.register(Transporte)
+admin.site.register(VeiculoTipo)
+admin.site.register(TransporteTipo)
+admin.site.register(TransporteCobertura)
--- agendamento/models.py
+++ agendamento/models.py
@@ -2,12 +2,126 @@ from django.db import models
 
 # Create your models here.
 
-class Agendamento(models.Model):
-    cliente = models.CharField(max_length=200, null=False)
+
+
+
+class Cooperativa(models.Model):
+    cooperativa = models.CharField(max_length=200, null=False)
     agencia = models.CharField(max_length=200, null=False)
-    inicio_vigencia = models.DateField(("Date"), blank=False)
-    fim_vigencia = models.DateField(("Date"), blank=False)
-    acao = models.TextField(max_length=500, null=True)
+    solicitante = models.CharField(max_length=200, null=False)
+    email = models.EmailField(max_length=100, null=False)
+    telefone = models.CharField(max_length=50, null=True)
+    celular = models.CharField(max_length=50, null=True)
+
 
     def __str__(self):
-        return  self.cliente
+        return  self.cooperativa
+
+
+class Segurado(models.Model):
+    nome = models.CharField(max_length=200, null=False)
+    email = models.EmailField(max_length=100, null=False)
+    telefone = models.CharField(max_length=50, null=True)
+    celular = models.CharField(max_length=50, null=True)
+    cnpj = models.CharField(max_length=14, null=False)
+    endereco = models.CharField(max_length=200, null= False)
+    cidade = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return  self.nome
+
+
+class Ramo(models.Model):
+    nome = models.CharField(max_length=200, null=False)
+
+    def __str__(self):
+        return self.nome
+
+class Empresarial(models.Model):
+    atividade = models.CharField(max_length=200, null=False)
+    qnt_local_risco = models.IntegerField(null=False)
+    IS = models.FloatField(null=False)
+    renovacao_cia = models.CharField(max_length=200)
+    final_vigencia = models.DateField(("Date"), null=False )
+
+
+class VidaGrupo(models.Model):
+    TYPE_CHOICES = (
+        ('GLOBAL', 'Global',),
+        ('ESPECIFICA', 'Específica',),
+    )
+    tipo = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    IS = models.FloatField(null=False)
+    atividade = models.CharField(max_length=200, null=False)
+    qnt_vida_seg = models.IntegerField(null=False)
+    renovacao_cia = models.CharField(max_length=200)
+    final_vigencia = models.DateField(("Date"), null=False )
+
+class VeiculoTipo(models.Model):
+    CHOICES = (
+        ('Leve', 'Leve'),
+        ('Pesado', 'Pesado'),
+    )
+    name = models.CharField(max_length=20,choices=CHOICES, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Frota(models.Model):
+    tipo_empresa = models.ManyToManyField(VeiculoTipo)
+    qnt_itens_seg = models.IntegerField(null=False)
+    renovacao_cia = models.CharField(max_length=200)
+    final_vigencia = models.DateField(("Date"), null=False)
+
+
+class TransporteTipo(models.Model):
+    CHOICES = (
+        ('Transportadora', 'Transportadora'),
+        ('Comum', 'Empresa Comum'),
+        ('Nacional', 'Nacional'),
+        ('Internacional', 'Internacional'),
+    )
+    name = models.CharField(max_length=20,choices=CHOICES, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class TransporteCobertura(models.Model):
+    CHOICES = (
+        ('Acidente', 'Acidente'),
+        ('Roubo', 'Roubo'),
+    )
+    name = models.CharField(max_length=20,choices=CHOICES, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Transporte(models.Model):
+    tipo_empresa = models.ManyToManyField(TransporteTipo)
+    cobertura = models.ManyToManyField(TransporteCobertura)
+    IS = models.FloatField(null=False)
+    mercadoria_transportada = models.CharField(max_length=200, null=False)
+    renovacao_cia = models.CharField(max_length=200)
+    final_vigencia = models.DateField(("Date"), null=False)
+
+
+class Produto(models.Model):
+    ramo = models.ForeignKey(Ramo, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=200, null=False)
+
+    def __str__(self):
+        return self.nome
+
+class Agendamento(models.Model):
+    cooperativa = models.ForeignKey(Cooperativa,on_delete=models.CASCADE)
+    segurado = models.ForeignKey(Segurado,on_delete=models.CASCADE)
+    ramo = models.ForeignKey(Ramo,on_delete=models.CASCADE)
+    colaborador = models.CharField(max_length=200)
+    pa = models.CharField(max_length=200)
+    inclusao = models.DateField(("Date"), null=False)
+    observacao = models.TextField(max_length=500)
+
+    def __str__(self):
+        title = self.cooperativa + " - " + self.segurado + " - " + self.ramo
+        return title
