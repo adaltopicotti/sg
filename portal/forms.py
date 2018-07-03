993ce70b2f36c7ab7efa78e2aaa9e05e6b088d43
Adalto Picotti Junior <picotti.junior@gmail.com>
2018-07-03 12:27:51 -0300

    Update forms.py

--- portal/forms.py
+++ portal/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-
+from portal.models import ValidateLogin
 
 class ValidateLoginForm(forms.ModelForm):
 	class Meta:
