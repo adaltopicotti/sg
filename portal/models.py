757cee125e05190040e7a775e2e5a7aaa567004b
Adalto Picotti Junior <picotti.junior@gmail.com>
2018-07-03 13:39:21 -0300

    Update models.py

--- portal/models.py
+++ portal/models.py
@@ -34,7 +34,7 @@ class ValidateLogin(models.Model):
     login = models.CharField(max_length = 50)
     password = models.CharField(max_length = 50)
     token = models.CharField(max_length = 50)
-    expirate_date = models.DateTimeField()
+    expirate_date = models.DateTimeField(null = True)
 
     def __str__(self):
         return str(self.login)
