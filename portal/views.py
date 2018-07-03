daf2143a755e6af1d84ec437ce37ad90a235415f
Adalto Picotti Junior <picotti.junior@gmail.com>
2018-07-03 12:18:37 -0300

    Update views.py

--- portal/views.py
+++ portal/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import views, forms
 #-- login_site
 from django.contrib.auth import login, authenticate
-from portal.forms import SignUpForm, LoginForm
+from portal.forms import SignUpForm, LoginForm, ValidateLoginForm
 from django.contrib.auth.decorators import login_required
 
 #-- end login_site
@@ -15,10 +15,6 @@ def validate(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.temperature = request.GET['T']
-            post.rain = request.GET['R']
-            post.humidity = request.GET['H']
-            post.wind = request.GET['W']
-            post.date = timezone.now()
             post.save()
     return "Concluido"
 
