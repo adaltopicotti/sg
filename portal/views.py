82e79c436a3891458a3f8c0b25339dc319561bb6
Adalto Picotti Junior <picotti.junior@gmail.com>
2018-07-03 14:07:08 -0300

    Update views.py

--- portal/views.py
+++ portal/views.py
@@ -20,7 +20,7 @@ def validate(request):
         form.token = request.GET['token']
         valid_user = ValidateLogin.objects.get(login=form.login)
         data = serializers.serialize("json", [valid_user,])
-    return HttpResponse(data.model)
+    return HttpResponse(data)
 
 
 def home(request):
